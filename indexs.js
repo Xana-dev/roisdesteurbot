030e5edb022cdb23a4f06a11b6de2ed9a20cef45
Xana-dev <70415899+Xana-dev@users.noreply.github.com>
2021-10-13 22:07:45 +0200

    Add files via upload

---
+++ indexs.js
@@ -0,0 +1,20 @@
+newFunction();
+
+function newFunction() {
+    const Discord = require("discord.js");
+    client; const nouveau, Discord, Client; 
+
+    cliente.sur("prêt", () => {
+        console.connecte("Je suis prêt!");
+    });
+
+    cliente.on(" message ", (message) => {
+        if (message.contenu.commenceAvec(" ping "))
+            ;
+    }); {
+        message.canal.envoyer(" pong ! ) ; ");
+    }
+    ;
+
+    cliente.login("SuperSecretBotTokenHere");
+}
